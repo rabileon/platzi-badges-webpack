1a8aaef0efdbbca2dc2c5e7097506ecc6e707a2c
rabi <rabi_leon@hotmail.com>
2020-09-14 22:47:24 -0500

    :bug: archivos faltantes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,5 @@ import './global.css';
 import App from './components/App';
 
 const container = document.getElementById('app');
-function render() {
-  ReactDOM.render(<App />, container);
-}
-render()
 
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    render()
-  })
-}
+ReactDOM.render(<App />, container);
